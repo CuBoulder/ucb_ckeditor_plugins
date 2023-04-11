0d3d69ee27ce55bbe5859d5a27d8aae9c69b7c1a
Pat <breepatbree@gmail.com>
2023-04-11 14:53:01 -0600

    wip: Removes nonfunctional dropdowns, finally get UI cloud

--- js/ckeditor5_plugins/button/src/buttonui.js
+++ js/ckeditor5_plugins/button/src/buttonui.js
@@ -1,52 +1,49 @@
 import { Plugin } from 'ckeditor5/src/core';
+import { ButtonView, ContextualBalloon, clickOutsideHandler } from 'ckeditor5/src/ui';
 import FormView from './buttonview';
-import { WidgetToolbarRepository } from 'ckeditor5/src/widget';
+import getRangeText from './buttonutils.js';
 import icon from '../../../../icons/hand-pointer-regular.svg';
-import { ButtonView, ContextualBalloon } from 'ckeditor5/src/ui';
-import getRangeText from './buttonutils';
 
 export default class ButtonUI extends Plugin {
-    static get requires() {
-		return [ContextualBalloon]
+	static get requires() {
+		return [ ContextualBalloon ];
 	}
-    init() {
-        const editor = this.editor;
-        
-        // TO DO - _createFormView currently errors out, t is not a function on View create. Need to solve UI... 
-        this._balloon = this.editor.plugins.get(ContextualBalloon);
-        // this.formView = this._createFormView();
 
-        editor.ui.componentFactory.add( 'button', () => {
-            const command = editor.commands.get( 'addButton' );
-            const button = new ButtonView();
-
-            button.set( {
-                label: 'Button',
-                icon: icon,
-                tooltip: true,
-            } );
-
-            this.listenTo( button, 'execute', () => {
-                this._showUI();
-                // editor.execute('addButton')
-            });
-
-            return button;
-        } );
-    }
-
-    _createFormView() {
+	init() {
 		const editor = this.editor;
-		const formView = new FormView( editor.locale ); // currently causes t is not a function
+
+        // Create the balloon and the form view.
+		this._balloon = this.editor.plugins.get( ContextualBalloon );
+		this.formView = this._createFormView();
+
+		editor.ui.componentFactory.add( 'button', () => {
+			const button = new ButtonView();
+
+			button.label = 'Button';
+			button.icon = icon;
+			button.tooltip = true;
+			button.withText = false;
+			
+			// Show the UI on button click.
+			this.listenTo( button, 'execute', () => {
+				this._showUI();
+			} );
+
+			return button;
+		} );
+	}
+
+  
+
+	_createFormView() {
+		const editor = this.editor;
+		const formView = new FormView( editor.locale );
 
 		// Execute the command after clicking the "Save" button.
 		this.listenTo( formView, 'submit', () => {
-			// Grab values from the color, style, size and link input fields.
+			// Grab values from the tool tip and title input fields.
 			const value = {
-				color: formView.colorSelect.fieldView.element.value,
-				style: formView.styleSelect.fieldView.element.value,
-                size: formView.sizeSelect.fieldView.element.value,
-                link: formView.linkInput.fieldView.element.value
+				link: formView.linkInputView.fieldView.element.value
 			};
 			editor.execute( 'addButton', value );
 
@@ -70,8 +67,8 @@ export default class ButtonUI extends Plugin {
 		return formView;
 	}
 
-    _showUI(){
-        const selection = this.editor.model.document.selection;
+	_showUI() {
+		const selection = this.editor.model.document.selection;
 
 		// Check the value of the command.
 		const commandValue = this.editor.commands.get( 'addButton' ).value;
@@ -81,37 +78,38 @@ export default class ButtonUI extends Plugin {
 			position: this._getBalloonPositionData()
 		} );
 
-		console.log
+		// Disable the input when the selection is not collapsed.
+		this.formView.linkInputView.isEnabled = selection.getFirstRange().isCollapsed;
 
 		// Fill the form using the state (value) of the command.
 		if ( commandValue ) {
-			this.formView.colorSelect.fieldView.value = commandValue.color;
-			this.formView.styleSelect.fieldView.value = commandValue.title;
-            this.formView.sizeSelect.fieldView.value = commandValue.size;
-            this.formView.linkInput.fieldView.value = commandValue.link
+			this.formView.linkInputView.fieldView.value = commandValue.link;
 		}
-
+		// If the command has no value, put the currently selected text (not collapsed)
+		// in the first field and empty the second in that case.
 		else {
 			const selectedText = getRangeText( selection.getFirstRange() );
-			this.formView.linkInput.fieldView.value = selectedText;
+
+			this.formView.linkInputView.fieldView.value = selectedText;
 		}
 
 		this.formView.focus();
-    }
+	}
 
-    _hideUI() {
+	_hideUI() {
 		// Clear the input field values and reset the form.
-        this.formView.linkInput.fieldView.value = ''
+		this.formView.linkInputView.fieldView.value = '';
+		// this.formView.titleInputView.fieldView.value = '';
 		this.formView.element.reset();
 
 		this._balloon.remove( this.formView );
 
-		// Focus the editing view after inserting the abbreviation so the user can start typing the content
+		// Focus the editing view after inserting the tooltip so the user can start typing the content
 		// right away and keep the editor focused.
 		this.editor.editing.view.focus();
 	}
 
-    _getBalloonPositionData() {
+	_getBalloonPositionData() {
 		const view = this.editor.editing.view;
 		const viewDocument = view.document;
 		let target = null;
--- js/ckeditor5_plugins/button/src/buttonview.js
+++ js/ckeditor5_plugins/button/src/buttonview.js
@@ -1,144 +1,115 @@
-import { 
-	View, 
-	LabeledFieldView, 
-	createDropdown, 
-	ButtonView, 
-	submitHandler, 
-	FocusCycler 
+import {
+	View,
+	LabeledFieldView,
+	createLabeledInputText,
+	ButtonView,
+	submitHandler,
+	FocusCycler
 } from 'ckeditor5/src/ui';
 import { FocusTracker, KeystrokeHandler } from 'ckeditor5/src/utils';
 import { icons } from 'ckeditor5/src/core';
 
 export default class FormView extends View {
-		constructor( locale ) {
-			super( locale );
-	
-			this.focusTracker = new FocusTracker();
-			this.keystrokes = new KeystrokeHandler();
-	
-			// Create a labeled field view for the color select.
-			this.colorSelectView = new LabeledFieldView( locale );
-			this.colorSelectView.label = 'Color';
-	
-			// Create the color select field.
-			this.colorSelect = createDropdown( this.locale, {
-				choices: [
-					{ value: 'blue', label: 'Blue' },
-					{ value: 'black', label: 'Black' },
-					{ value: 'grey', label: 'Grey' },
-					{ value: 'white', label: 'White' }
-				]
-			} );
-			this.colorSelectView.fieldView = this.colorSelect;
-	
-			// Create a labeled field view for the style select.
-			this.styleSelectView = new LabeledFieldView( locale );
-			this.styleSelectView.label = 'Style';
-	
-			// Create the style select field.
-			this.styleSelect = createDropdown( this.locale, {
-				choices: [
-					{ value: 'Regular', label: 'Regular' },
-					{ value: 'Full', label: 'Full' }
-				]
-			} );
-			this.styleSelectView.fieldView = this.styleSelect;
-	
-			// Create a labeled field view for the size select.
-			this.sizeSelectView = new LabeledFieldView( locale );
-			this.sizeSelectView.label = 'Size';
-	
-			// Create the size select field.
-			this.sizeSelect = createDropdown( this.locale, {
-				choices: [
-					{ value: 'Large', label: 'Large' },
-					{ value: 'Regular', label: 'Regular' },
-					{ value: 'Small', label: 'Small' }
-				]
-			} );
-			this.sizeSelectView.fieldView = this.sizeSelect;
-	
-			// Create a labeled field view for the link input.
-			this.linkView = new LabeledFieldView( locale );
-			this.linkView.label = 'Link';
-	
-			// Create the link input field.
-			this.linkInput = document.createElement('input');
-			this.linkInput.type = 'text';
-			this.linkInput.placeholder = 'Enter link URL here';
-			this.linkView.fieldView = this.linkInput;
-	
-			// Create the save button view.
-			this.saveButtonView = new ButtonView( locale );
-			this.saveButtonView.label = 'Save';
-			this.saveButtonView.icon = icons.check
-			this.saveButtonView.class = 'ck-button-save';
-	
-			// Set the type of the save button to "submit" to trigger the submit event on the entire form when clicked.
-			this.saveButtonView.type = 'submit';
-	
-			// Create the cancel button view.
-			this.cancelButtonView = new ButtonView( locale );
-			this.cancelButtonView.label = 'Cancel';
-			this.cancelButtonView.icon = icons.cancel;
-			this.cancelButtonView.class = 'ck-button-cancel';
-	
-			// Set the type of the cancel button to "button" to prevent it from triggering the form submission.
-			this.cancelButtonView.set({ type: 'button' });
+	constructor( locale ) {
+		super( locale );
 
-			this.childViews = this.createCollection([
-				this.linkInput,
-				this.sizeSelect,
-				this.colorSelect,
-				this.styleSelect,
-				this.saveButtonView,
-				this.cancelButtonView
-			])
+		this.focusTracker = new FocusTracker();
+		this.keystrokes = new KeystrokeHandler();
 
-			this._focusCycler = new FocusCycler( {
-				focusables: this.childViews,
-				focusTracker: this.focusTracker,
-				keystrokeHandler: this.keystrokes,
-				actions: {
-					// Navigate form fields backwards using the Shift + Tab keystroke.
-					focusPrevious: 'shift + tab',
-	
-					// Navigate form fields forwards using the Tab key.
-					focusNext: 'tab'
-				}
-			} );
-	
-			this.setTemplate( {
-				tag: 'form',
-				attributes: {
-					class: [ 'ck', 'ck-button-form' ],
-					tabindex: '-1'
-				},
-				children: this.childViews
-			} );
-		}
-	
-		render() {
-			super.render();
-	
-			submitHandler( {
-				view: this
-			} );
-	
-			this.childViews._items.forEach( view => {
-				// Register the view in the focus tracker.
-				this.focusTracker.add( view.element );
-			} );
-	
-			// Start listening for the keystrokes coming from #element.
-			this.keystrokes.listenTo( this.element );
-		}
+		this.linkInputView = this._createInput( 'Add Link' );
 
-		destroy() {
-			super.destroy();
-	
-			this.focusTracker.destroy();
-			this.keystrokes.destroy();
+		this.saveButtonView = this._createButton( 'Save', icons.check, 'ck-button-save' );
+
+		// Submit type of the button will trigger the submit event on entire form when clicked 
+		//(see submitHandler() in render() below).
+		this.saveButtonView.type = 'submit';
+
+		this.cancelButtonView = this._createButton( 'Cancel', icons.cancel, 'ck-button-cancel' );
+
+		// Delegate ButtonView#execute to FormView#cancel.
+		this.cancelButtonView.delegate( 'execute' ).to( this, 'cancel' );
+
+		this.childViews = this.createCollection( [
+			this.linkInputView,
+			this.saveButtonView,
+			this.cancelButtonView
+		] );
+
+		this._focusCycler = new FocusCycler( {
+			focusables: this.childViews,
+			focusTracker: this.focusTracker,
+			keystrokeHandler: this.keystrokes,
+			actions: {
+				// Navigate form fields backwards using the Shift + Tab keystroke.
+				focusPrevious: 'shift + tab',
+
+				// Navigate form fields forwards using the Tab key.
+				focusNext: 'tab'
+			}
+		} );
+
+		this.setTemplate( {
+			tag: 'form',
+			attributes: {
+				class: [ 'ck', 'ck-button-form' ],
+				tabindex: '-1'
+			},
+			children: this.childViews
+		} );
+	}
+
+	render() {
+		super.render();
+
+		submitHandler( {
+			view: this
+		} );
+
+		this.childViews._items.forEach( view => {
+			// Register the view in the focus tracker.
+			this.focusTracker.add( view.element );
+		} );
+
+		// Start listening for the keystrokes coming from #element.
+		this.keystrokes.listenTo( this.element );
+	}
+
+	destroy() {
+		super.destroy();
+
+		this.focusTracker.destroy();
+		this.keystrokes.destroy();
+	}
+
+	focus() {
+		// If the abbreviation text field is enabled, focus it straight away to allow the user to type.
+		if ( this.linkInputView.isEnabled ) {
+			this.linkInputView.focus();
 		}
-			
+		// Focus the tooltip title field if the former is disabled.
+		else {
+			this.linkInputView.focus();
+		}
+	}
+
+	_createInput( label ) {
+		const labeledInput = new LabeledFieldView( this.locale, createLabeledInputText );
+
+		labeledInput.label = label;
+
+		return labeledInput;
+	}
+
+	_createButton( label, icon, className ) {
+		const button = new ButtonView();
+
+		button.set( {
+			label,
+			icon,
+			tooltip: true,
+			class: className
+		} );
+
+		return button;
+	}
 }
