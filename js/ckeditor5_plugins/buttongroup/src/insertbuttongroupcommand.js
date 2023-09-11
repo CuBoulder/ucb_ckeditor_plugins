23e762343b8cadbfc4d1ca7b59e421ece7bb84d3
Pat <breepatbree@gmail.com>
2023-09-11 16:41:58 -0600

    wip: sort of updates, needs work

--- js/ckeditor5_plugins/buttongroup/src/insertbuttongroupcommand.js
+++ js/ckeditor5_plugins/buttongroup/src/insertbuttongroupcommand.js
@@ -41,6 +41,24 @@ export default class ButtonGroupCommand extends Command {
 		const selection = model.document.selection;
 		const selectedElement = selection.getSelectedElement();
 
+		console.log(selectedElement)
+
+
+		if(selectedElement && selectedElement.name == 'buttonGroup'){
+			const color = selectedElement.getAttribute('buttonGroupColor')
+			const size = selectedElement.getAttribute('buttonGroupSize')
+
+			console.log('my color',color)
+			console.log('my size', size)
+			const childBtns = Array.from(selectedElement.getChildren())
+			childBtns.forEach(btn=>{
+				console.log('button', btn)
+				btn._setAttribute('linkButtonColor', color)
+				btn._setAttribute('linkButtonSize', size)
+			})
+
+		}
+
 		const allowedIn = model.schema.findAllowedParent(
 			selection.getFirstPosition(),
 			'buttonGroup'
