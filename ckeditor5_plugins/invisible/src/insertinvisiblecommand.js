3807ef9908f6270187ee54dd2a4888b42fade6f6
Pat <breepatbree@gmail.com>
2023-11-06 14:19:22 -0700

    wip: Add/Remove working, ability to esc working. Needs remove fix

--- ckeditor5_plugins/invisible/src/insertinvisiblecommand.js
+++ ckeditor5_plugins/invisible/src/insertinvisiblecommand.js
@@ -1,65 +1,108 @@
-/**
- * @file defines insertInvisibleCommand, which is executed when the Invisible
- * toolbar button is pressed.
- */
-// cSpell:ignore simpleboxeditingimport { Command } from 'ckeditor5/src/core';
 import { Command } from 'ckeditor5/src/core';
-import { IconView } from 'ckeditor5/src/ui';
-
-
-// const selection = editor.model.document.selection;
-
 
 export default class InvisibleCommand extends Command {
+  constructor( editor ) {
+    super( editor );
+
+    // Listen for the Enter key and execute the command if conditions are met.
+    editor.keystrokes.set( 'Enter', ( data, cancel ) => {
+      const model = editor.model;
+      const selection = model.document.selection;
+
+      // If the selection is in an invisible element, let's exit that element.
+      if ( selection.focus && selection.focus.parent.name === 'ucb-invisible' ) {
+        model.change( writer => {
+          // Insert a paragraph after the invisible element.
+          const paragraph = writer.createElement( 'paragraph' );
+          writer.insert( paragraph, selection.focus.parent, 'after' );
+
+          // Place the selection into the new paragraph.
+          writer.setSelection( paragraph, 'in' );
+        });
+
+        // Prevent the default action of adding a new line in the invisible element.
+        cancel();
+      }
+    }, { priority: 'high' } ); // Use high priority to override the default Enter key behavior.
+  }
+
   execute() {
     const model = this.editor.model;
     const selection = model.document.selection;
+    const invisibleElement = findInvisibleElement(selection);
 
+    console.log('invisible element', invisibleElement)
     model.change(writer => {
-      // Insert <ucb-invisible>*</ucb-invisible> at the current selection position
-      // in a way that will result in creating a valid model structure.
-
-      // const selectedText = this.editor.getData({ selection: true });
-      const invisible = addInvisible(writer, selection);
-      model.insertContent(invisible);
+      if (invisibleElement) {
+        // If selection is in an invisible element, remove the element.
+        removeInvisible(writer, invisibleElement);
+      } else {
+        // Otherwise, add an invisible element.
+        const invisible = addInvisible(writer, selection);
+        model.insertContent(invisible);
+      }
     });
   }
 
   refresh() {
     const model = this.editor.model;
     const selection = model.document.selection;
+    
+   // Check if the command should be enabled based on the current selection.
+   const allowedIn = model.schema.findAllowedParent(
+    selection.getFirstPosition(),
+    'ucb-invisible'
+  );
 
-    // Determine if the cursor (selection) is in a position where adding a
-    // Invisible is permitted. This is based on the schema of the model(s)
-    // currently containing the cursor.
-    const allowedIn = model.schema.findAllowedParent(
-      selection.getFirstPosition(),
-      'ucb-invisible'
-    );
+  // The command is enabled if we're in a position to insert an invisible element.
+  // The command's value is true if the selection is inside an invisible element.
+  const isInvisible = findInvisibleElement(selection);
 
-    // If the cursor is not in a location where a ucb-invisible can be added, return
-    // null so the addition doesn't happen.
-    this.isEnabled = allowedIn !== null;
+  this.isEnabled = allowedIn !== null || isInvisible !== null;
+  this.value = !!isInvisible;
   }
 }
 
-function addInvisible(writer, text) {
-  // Create instances of the element registered with the editor.
+function findInvisibleElement(selection) {
+  // Ensure there's a focus position to look at
+  if (!selection.focus) {
+    return null;
+  }
 
-  // Make the element
-  const invisible = writer.createElement('ucb-invisible');
-  // Strip the tags out of the selected text, convert to text Node for append
-  
+  // Check the name of the focus parent, if it's 'ucb-invisible', return it
+  if (selection.focus.parent && selection.focus.parent.name === 'ucb-invisible') {
+    return selection.focus.parent;
+  }
 
-  const range = text.getFirstRange();
-
-  for (const item of range.getItems()) {
-    const textNode = writer.createText(item.data)
-    writer.append(textNode, invisible)
-
-  }  
-
-  // Return the element to be added to the editor.
-  return invisible;
+  // If the parent is not 'ucb-invisible', return null and log an error for debugging
+  return null;
 }
 
+
+function removeInvisible( writer, invisibleElement ) {
+  const range = writer.createRangeOn(invisibleElement);
+  const contents = range.getContainedElement();
+  const parent = invisibleElement.parent
+  console.log(parent)
+  writer.unwrap(invisibleElement);
+  // Move selection out of the now-removed invisible element
+  if (contents) {
+    writer.setSelection(contents, 'after');
+  }
+}
+
+
+
+
+function addInvisible( writer, selection ) {
+  const invisible = writer.createElement( 'ucb-invisible' );
+
+  const range = selection.getFirstRange();
+
+  for ( const item of range.getItems() ) {
+    const textNode = writer.createText( item.data );
+    writer.append( textNode, invisible );
+  }
+
+  return invisible;
+}
--- ckeditor5_plugins/invisible/src/invisibleui.js
+++ ckeditor5_plugins/invisible/src/invisibleui.js
@@ -7,26 +7,26 @@ import audioIcon from '../../../icons/invisible.svg'
 export default class InvisibleUI extends Plugin {
 	init() {
 		const editor = this.editor;
-
-		// Add strikethrough button to feature components.
-		editor.ui.componentFactory.add( 'invisible', () => {
-			const command = editor.commands.get('addInvisible')
-			const button = new ButtonView();
-
-			button.label = 'Screen-Reader Only';
-			button.icon = audioIcon;
-			button.tooltip = true;
-			button.withText = false;
-
-
-			button.bind('isOn', 'isEnabled').to(command, 'value', 'isEnabled');
-			// Execute the command when the button is clicked (executed).
-			this.listenTo(button, 'execute', () =>
-				editor.execute('addInvisible'),
-			);
-
-			return button;
-		} );
-	}
+		editor.ui.componentFactory.add('invisible', locale => {
+		  const command = editor.commands.get('addInvisible');
+		  const button = new ButtonView(locale);
+	
+		  button.set({
+			label: 'Screen-Reader Only',
+			icon: audioIcon,
+			tooltip: true
+		  });
+	
+		  button.bind('isOn', 'isEnabled').to(command, 'value', 'isEnabled');
+	
+		  // Execute the command when the button is clicked (executed).
+		  this.listenTo(button, 'execute', () => {
+			editor.execute('addInvisible');
+			editor.editing.view.focus();
+		  });
+	
+		  return button;
+		});
+	  }
 }
 	
