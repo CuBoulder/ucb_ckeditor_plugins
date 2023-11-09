1149a2271f3c5a9811c7d6e2046a9d8bd1ac8f6c
Pat <breepatbree@gmail.com>
2023-11-09 15:01:31 -0700

    Adds invisible widget form for input

--- ckeditor5_plugins/invisible/src/invisibleui.js
+++ ckeditor5_plugins/invisible/src/invisibleui.js
@@ -1,33 +1,67 @@
-// Handles the admin side where youre adding buttons to toolbar
-// Also handles the format and display during editing
+// // Handles the admin side where youre adding buttons to toolbar
+// // Also handles the format and display during editing
 import { Plugin } from 'ckeditor5/src/core';
-import { ButtonView } from 'ckeditor5/src/ui';
+import { WidgetToolbarRepository } from 'ckeditor5/src/widget';
+import { addToolbarToDropdown, ButtonView, createDropdown, createLabeledInputText, FocusCycler, LabeledFieldView, submitHandler, View } from 'ckeditor5/src/ui';
 import audioIcon from '../../../icons/invisible.svg'
 
 export default class InvisibleUI extends Plugin {
+	static get requires() {
+		return [ WidgetToolbarRepository ];
+	}
+
 	init() {
 		const editor = this.editor;
-		editor.ui.componentFactory.add('invisible', locale => {
-		  const command = editor.commands.get('addInvisible');
-		  const button = new ButtonView(locale);
-	
-		  button.set({
-			label: 'Screen-Reader Only',
-			icon: audioIcon,
-			tooltip: true
-		  });
-	
-		  button.bind('isOn', 'isEnabled').to(command, 'value', 'isEnabled');
-	
-		  // Execute the command when the button is clicked (executed).
-		  this.listenTo(button, 'execute', () => {
-			editor.execute('addInvisible');
-			editor.editing.view.focus();
-		  });
-	
-		  return button;
-		});
-	  }
+		const commands = editor.commands;
+		const componentFactory = editor.ui.componentFactory;
+
+		// This will register the ButtonGroup button to the toolbar
+		componentFactory.add( 'buttonGroup', (locale) => {
+			const button = new ButtonView(locale);
+			const command = commands.get('addInvisible');
+		
+			button.label = 'Screen-Reader Only';
+			button.icon = audioIcon
+			button.tooltip = true;
+			button.withText = false;
+			button.isToggleable = true;
+
+			// Bind the state of the button to the command.
+			button.bind('isOn', 'isEnabled').to(command, 'value', 'isEnabled');
+
+			// Execute the command when the button is clicked (executed).
+			this.listenTo(button, 'execute', () => editor.execute('addInvisible'));
+
+			return button;
+		} );
+		componentFactory.add('invisibleInnerText', locale =>
+			this._createInput(locale, 'Screen-Reader Text'));
+	}
+
+		/**
+	 * @inheritdoc
+	 */
+		afterInit() {
+			const editor = this.editor;
+			const widgetToolbarRepository = editor.plugins.get(WidgetToolbarRepository);
+		
+			widgetToolbarRepository.register('invisible', {
+				items: ['invisibleInnerText'],
+				getRelatedElement: (selection) => {
+					const selectedElement = selection.getSelectedElement();
+					if (selectedElement && selectedElement.is('element') && selectedElement.hasClass('ucb-invisible'))
+						return selectedElement;
+				  	return null;
+				}
+			});
+		}
+		_createInput(locale, label) {
+			const labeledInput = new LabeledFieldView(locale, createLabeledInputText);
+			labeledInput.label = locale.t(label);
+			return labeledInput;
+		}
 }
+
+
 
 	
