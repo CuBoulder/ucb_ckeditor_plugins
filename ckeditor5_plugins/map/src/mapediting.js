14331be6d0e1f2bac1934ea71e25bb574ff8c026
Timur Tripp <timur@timurtripp.com>
2024-05-28 12:44:29 -0600

    CuBoulder/ucb_ckeditor_plugins#64 Updates map plugin

--- ckeditor5_plugins/map/src/mapediting.js
+++ ckeditor5_plugins/map/src/mapediting.js
@@ -30,152 +30,152 @@ import { campusMapLocationToURL, googleMapLocationToURL } from './maputils';
  * converted to standard DOM markup.
  */
 export default class MapEditing extends Plugin {
-	/**
-	 * @inheritdoc
-	 */
-	static get requires() {
-		return [Widget];
-	}
+  /**
+   * @inheritdoc
+   */
+  static get requires() {
+    return [Widget];
+  }
 
-	/**
-	 * @inheritdoc
-	 */
-	init() {
-		this._defineSchema();
-		this._defineConverters();
-		this._defineCommands();
-	}
+  /**
+   * @inheritdoc
+   */
+  init() {
+    this._defineSchema();
+    this._defineConverters();
+    this._defineCommands();
+  }
 
-	/*
-	 * This registers the structure that will be seen by CKEditor 5 as
-	 * <campusMap>
-	 * </campusMap>
-	 *
-	 * The logic in _defineConverters() will determine how this is converted to
-	 * markup.
-	 */
-	_defineSchema() {
-		// Schemas are registered via the central `editor` object.
-		const schema = this.editor.model.schema;
+  /*
+   * This registers the structure that will be seen by CKEditor 5 as
+   * <campusMap>
+   * </campusMap>
+   *
+   * The logic in _defineConverters() will determine how this is converted to
+   * markup.
+   */
+  _defineSchema() {
+    // Schemas are registered via the central `editor` object.
+    const schema = this.editor.model.schema;
 
-		schema.register('campusMap', {
-			// Behaves like a self-contained object (e.g. an image).
-			isObject: true,
-			// Allow in places where other blocks are allowed (e.g. directly in the root).
-			allowWhere: '$block',
-			// Allow the attributes which control the map's alignment, style, and theme.
-			allowAttributes: ['mapLocation', 'mapSize'],
-			// Disallows any child elements inside the <ucb-map> element.
-			allowChildren: false
-		});
+    schema.register('campusMap', {
+      // Behaves like a self-contained object (e.g. an image).
+      isObject: true,
+      // Allow in places where other blocks are allowed (e.g. directly in the root).
+      allowWhere: '$block',
+      // Allow the attributes which control the map's alignment, style, and theme.
+      allowAttributes: ['mapLocation', 'mapSize'],
+      // Disallows any child elements inside the <ucb-map> element.
+      allowChildren: false
+    });
 
-		schema.register('googleMap', {
-			// Behaves like a self-contained object (e.g. an image).
-			isObject: true,
-			// Allow in places where other blocks are allowed (e.g. directly in the root).
-			allowWhere: '$block',
-			// Allow the attributes which control the map's alignment, style, and theme.
-			allowAttributes: ['mapLocation', 'mapSize'],
-			// Disallows any child elements inside the <ucb-map> element.
-			allowChildren: false
-		});
-	}
+    schema.register('googleMap', {
+      // Behaves like a self-contained object (e.g. an image).
+      isObject: true,
+      // Allow in places where other blocks are allowed (e.g. directly in the root).
+      allowWhere: '$block',
+      // Allow the attributes which control the map's alignment, style, and theme.
+      allowAttributes: ['mapLocation', 'mapSize'],
+      // Disallows any child elements inside the <ucb-map> element.
+      allowChildren: false
+    });
+  }
 
-	/**
-	 * Converters determine how CKEditor 5 models are converted into markup and
-	 * vice-versa.
-	 */
-	_defineConverters() {
-		// Converters are registered via the central editor object.
-		const conversion = this.editor.conversion;
+  /**
+   * Converters determine how CKEditor 5 models are converted into markup and
+   * vice-versa.
+   */
+  _defineConverters() {
+    // Converters are registered via the central editor object.
+    const conversion = this.editor.conversion;
 
-		// Specifies the location attribute for campus maps.
-		conversion.attributeToAttribute({
-			model: 'mapLocation',
-			view: 'data-map-location'
-		});
+    // Specifies the location attribute for campus maps.
+    conversion.attributeToAttribute({
+      model: 'mapLocation',
+      view: 'data-map-location'
+    });
 
-		// The size attribute converts to element class names.
-		conversion.attributeToAttribute(buildAttributeToAttributeDefinition('mapSize', sizeOptions));
+    // The size attribute converts to element class names.
+    conversion.attributeToAttribute(buildAttributeToAttributeDefinition('mapSize', sizeOptions));
 
-		// Upcast Converters: determine how existing HTML is interpreted by the
-		// editor. These trigger when an editor instance loads.
-		//
-		// If <ucb-map class="ucb-map ucb-campus-map"> is present in the existing markup
-		// processed by CKEditor, then CKEditor recognizes and loads it as a
-		// <campusMap> model.
-		conversion.for('upcast').elementToElement({
-			model: 'campusMap',
-			view: {
-				name: 'ucb-map',
-				classes: ['ucb-map', 'ucb-campus-map']
-			}
-		});
+    // Upcast Converters: determine how existing HTML is interpreted by the
+    // editor. These trigger when an editor instance loads.
+    //
+    // If <ucb-map class="ucb-map ucb-campus-map"> is present in the existing markup
+    // processed by CKEditor, then CKEditor recognizes and loads it as a
+    // <campusMap> model.
+    conversion.for('upcast').elementToElement({
+      model: 'campusMap',
+      view: {
+        name: 'ucb-map',
+        classes: ['ucb-map', 'ucb-campus-map']
+      }
+    });
 
-		// Upcast Converters: determine how existing HTML is interpreted by the
-		// editor. These trigger when an editor instance loads.
-		//
-		// If <ucb-map class="ucb-map ucb-google-map"> is present in the existing markup
-		// processed by CKEditor, then CKEditor recognizes and loads it as a
-		// <googleMap> model.
-		conversion.for('upcast').elementToElement({
-			model: 'googleMap',
-			view: {
-				name: 'ucb-map',
-				classes: ['ucb-map', 'ucb-google-map']
-			}
-		});
+    // Upcast Converters: determine how existing HTML is interpreted by the
+    // editor. These trigger when an editor instance loads.
+    //
+    // If <ucb-map class="ucb-map ucb-google-map"> is present in the existing markup
+    // processed by CKEditor, then CKEditor recognizes and loads it as a
+    // <googleMap> model.
+    conversion.for('upcast').elementToElement({
+      model: 'googleMap',
+      view: {
+        name: 'ucb-map',
+        classes: ['ucb-map', 'ucb-google-map']
+      }
+    });
 
-		// Data Downcast Converters: converts stored model data into HTML.
-		// These trigger when content is saved.
-		//
-		// Instances of <campusMap> are saved as
-		// <ucb-map class="ucb-map ucb-campus-map"></ucb-map>.
-		conversion.for('dataDowncast').elementToElement({
-			model: 'campusMap',
-			view: (modelElement, { writer: viewWriter }) => createCampusMapView(modelElement, viewWriter)
-		});
+    // Data Downcast Converters: converts stored model data into HTML.
+    // These trigger when content is saved.
+    //
+    // Instances of <campusMap> are saved as
+    // <ucb-map class="ucb-map ucb-campus-map"></ucb-map>.
+    conversion.for('dataDowncast').elementToElement({
+      model: 'campusMap',
+      view: (modelElement, { writer: viewWriter }) => createCampusMapView(modelElement, viewWriter)
+    });
 
-		// Data Downcast Converters: converts stored model data into HTML.
-		// These trigger when content is saved.
-		//
-		// Instances of <campusMap> are saved as
-		// <ucb-map class="ucb-map ucb-google-map"></ucb-map>.
-		conversion.for('dataDowncast').elementToElement({
-			model: 'googleMap',
-			view: (modelElement, { writer: viewWriter }) => createGoogleMapView(modelElement, viewWriter)
-		});
+    // Data Downcast Converters: converts stored model data into HTML.
+    // These trigger when content is saved.
+    //
+    // Instances of <campusMap> are saved as
+    // <ucb-map class="ucb-map ucb-google-map"></ucb-map>.
+    conversion.for('dataDowncast').elementToElement({
+      model: 'googleMap',
+      view: (modelElement, { writer: viewWriter }) => createGoogleMapView(modelElement, viewWriter)
+    });
 
-		// Editing Downcast Converters. These render the content to the user for
-		// editing, i.e. this determines what gets seen in the editor. These trigger
-		// after the Data Upcast Converters, and are re-triggered any time there
-		// are changes to any of the models' properties.
-		//
-		// Convert the <campusMap> model into a container widget in the editor UI.
-		conversion.for('editingDowncast').elementToElement({
-			model: 'campusMap',
-			view: (modelElement, { writer: viewWriter }) => createCampusMapView(modelElement, viewWriter, true)
-		});
+    // Editing Downcast Converters. These render the content to the user for
+    // editing, i.e. this determines what gets seen in the editor. These trigger
+    // after the Data Upcast Converters, and are re-triggered any time there
+    // are changes to any of the models' properties.
+    //
+    // Convert the <campusMap> model into a container widget in the editor UI.
+    conversion.for('editingDowncast').elementToElement({
+      model: 'campusMap',
+      view: (modelElement, { writer: viewWriter }) => createCampusMapView(modelElement, viewWriter, true)
+    });
 
-		// Editing Downcast Converters. These render the content to the user for
-		// editing, i.e. this determines what gets seen in the editor. These trigger
-		// after the Data Upcast Converters, and are re-triggered any time there
-		// are changes to any of the models' properties.
-		//
-		// Convert the <googleMap> model into a container widget in the editor UI.
-		conversion.for('editingDowncast').elementToElement({
-			model: 'googleMap',
-			view: (modelElement, { writer: viewWriter }) => createGoogleMapView(modelElement, viewWriter, true)
-		});
-	}
+    // Editing Downcast Converters. These render the content to the user for
+    // editing, i.e. this determines what gets seen in the editor. These trigger
+    // after the Data Upcast Converters, and are re-triggered any time there
+    // are changes to any of the models' properties.
+    //
+    // Convert the <googleMap> model into a container widget in the editor UI.
+    conversion.for('editingDowncast').elementToElement({
+      model: 'googleMap',
+      view: (modelElement, { writer: viewWriter }) => createGoogleMapView(modelElement, viewWriter, true)
+    });
+  }
 
-	/**
-	 * Defines the commands for inserting or modifying the map.
-	 */
-	_defineCommands() {
-		const commands = this.editor.commands;
-		commands.add('insertMap', new InsertMapCommand(this.editor));
-	}
+  /**
+   * Defines the commands for inserting or modifying the map.
+   */
+  _defineCommands() {
+    const commands = this.editor.commands;
+    commands.add('insertMap', new InsertMapCommand(this.editor));
+  }
 }
 
 /**
@@ -187,16 +187,16 @@ export default class MapEditing extends Plugin {
  *   The attribute to attribute definition of the specified attribute.
  */
 function buildAttributeToAttributeDefinition(attributeName, attributeOptions) {
-	const view = {};
-	for (const [name, option] of Object.entries(attributeOptions))
-		view[name] = { key: 'class', value: option.className };
-	return {
-		model: {
-			key: attributeName,
-			values: Object.keys(attributeOptions)
-		},
-		view
-	};
+  const view = {};
+  for (const [name, option] of Object.entries(attributeOptions))
+    view[name] = { key: 'class', value: option.className };
+  return {
+    model: {
+      key: attributeName,
+      values: Object.keys(attributeOptions)
+    },
+    view
+  };
 }
 
 /**
@@ -210,22 +210,22 @@ function buildAttributeToAttributeDefinition(attributeName, attributeOptions) {
  *   The Campus Map element or widget.
  */
 function createCampusMapView(modelElement, downcastWriter, widget = false) {
-	if (widget) {
-		const mapLocation = (modelElement.getAttribute('mapLocation') || '').replace(/\D/g, '');
-		return toWidget(downcastWriter.createContainerElement('div',
-			{
-				class: 'ucb-map ucb-campus-map',
-				style: `background-image: url('https://staticmap.concept3d.com/map/static-map/?map=336&loc=${mapLocation}&scale=2')`
-			}, [
-			downcastWriter.createRawElement('a', {
-				href: campusMapLocationToURL(mapLocation)
-			}, element => {
-				element.innerHTML = '<span>View location on the Campus Map</span>';
-				element.onclick = ev => ev.preventDefault(); // Prevents following the link when clicking the widget.
-			})
-		]), downcastWriter, { label: 'map widget' });
-	}
-	return downcastWriter.createContainerElement('ucb-map', { class: 'ucb-map ucb-campus-map' });
+  if (widget) {
+    const mapLocation = (modelElement.getAttribute('mapLocation') || '').replace(/\D/g, '');
+    return toWidget(downcastWriter.createContainerElement('div',
+      {
+        class: 'ucb-map ucb-campus-map',
+        style: `background-image: url('https://staticmap.concept3d.com/map/static-map/?map=336&loc=${mapLocation}&scale=2')`
+      }, [
+      downcastWriter.createRawElement('a', {
+        href: campusMapLocationToURL(mapLocation)
+      }, element => {
+        element.innerHTML = '<span>View location on the Campus Map</span>';
+        element.onclick = ev => ev.preventDefault(); // Prevents following the link when clicking the widget.
+      })
+    ]), downcastWriter, { label: 'map widget' });
+  }
+  return downcastWriter.createContainerElement('ucb-map', { class: 'ucb-map ucb-campus-map' });
 }
 
 /**
@@ -239,21 +239,21 @@ function createCampusMapView(modelElement, downcastWriter, widget = false) {
  *   The Google Maps element or widget.
  */
  function createGoogleMapView(modelElement, downcastWriter, widget = false) {
-	if (widget) {
-		const mapLocation = modelElement.getAttribute('mapLocation') || '';
-		return toWidget(downcastWriter.createContainerElement('div',
-			{
-				class: 'ucb-map ucb-google-map',
-			}, [
-			downcastWriter.createEmptyElement('iframe', {
-				src: googleMapLocationToURL(mapLocation),
-				loading: 'lazy',
-				referrerpolicy: 'no-referrer'
-			}),
-			downcastWriter.createEmptyElement('div', {
-				class: 'ucb-map-editing-cover'
-			})
-		]), downcastWriter, { label: 'map widget' });
-	}
-	return downcastWriter.createContainerElement('ucb-map', { class: 'ucb-map ucb-google-map' });
+  if (widget) {
+    const mapLocation = modelElement.getAttribute('mapLocation') || '';
+    return toWidget(downcastWriter.createContainerElement('div',
+      {
+        class: 'ucb-map ucb-google-map',
+      }, [
+      downcastWriter.createEmptyElement('iframe', {
+        src: googleMapLocationToURL(mapLocation),
+        loading: 'lazy',
+        referrerpolicy: 'no-referrer'
+      }),
+      downcastWriter.createEmptyElement('div', {
+        class: 'ucb-map-editing-cover'
+      })
+    ]), downcastWriter, { label: 'map widget' });
+  }
+  return downcastWriter.createContainerElement('ucb-map', { class: 'ucb-map ucb-google-map' });
 }
--- ckeditor5_plugins/map/src/maputils.js
+++ ckeditor5_plugins/map/src/maputils.js
@@ -8,10 +8,10 @@
  *   The `src` of the embedded `<iframe>`, or `null` if there isn't one.
  */
 function embedCodeToURL(embedCode) {
-	const iframeElement = new DOMParser().parseFromString(embedCode, 'text/html').querySelector('iframe');
-	if (iframeElement)
-		return iframeElement.getAttribute('src');
-	return null;
+  const iframeElement = new DOMParser().parseFromString(embedCode, 'text/html').querySelector('iframe');
+  if (iframeElement)
+    return iframeElement.getAttribute('src');
+  return null;
 }
 
 /**
@@ -22,15 +22,17 @@ function embedCodeToURL(embedCode) {
  *   while passing the URL `'https://www.colorado.edu/'` returns `null` as it doesn't link directly to a Campus Map.
  */
 export function campusMapURLToLocation(url) {
-	if (url[0] === '<') {
-		url = embedCodeToURL(url); // Gets a URL from a likely embed code.
-		if (!url) return null;
-	}
-	let urlified;
-	try {
-		urlified = new URL(url);
-	} catch (e) { return null; }
-	return (urlified.hostname === 'colorado.edu' || urlified.hostname === 'www.colorado.edu') && (urlified.pathname === '/map' || urlified.pathname === '/map/') ? urlified.hash.replace(/\D+/, '') : null;
+  if (url[0] === '<') {
+    url = embedCodeToURL(url); // Gets a URL from a likely embed code.
+    if (!url) return null;
+  }
+  let urlified;
+  try {
+    urlified = new URL(url);
+  } catch (e) { return null; }
+  if ((urlified.hostname !== 'colorado.edu' && urlified.hostname !== 'www.colorado.edu') || (urlified.pathname !== '/map' && urlified.pathname !== '/map/') || !urlified.hash)
+    return null;
+  return urlified.hash.match(/m\/(\d+)/)[1];
 }
 
 /**
@@ -39,15 +41,17 @@ export function campusMapURLToLocation(url) {
  *   Just the location part of the URL, or null if the URL isn't a valid Google Maps URL.
  */
  export function googleMapURLToLocation(url) {
-	if (url[0] === '<') {
-		url = embedCodeToURL(url); // Gets a URL from a likely embed code.
-		if (!url) return null;
-	}
-	let urlified;
-	try {
-		urlified = new URL(url);
-	} catch (e) { return null; }
-	return (urlified.hostname === 'google.com' || urlified.hostname === 'www.google.com') && (urlified.pathname === '/maps/embed' || urlified.pathname === '/maps/embed/') ? decodeURIComponent(urlified.searchParams.get('pb') || '') || null : null;
+  if (url[0] === '<') {
+    url = embedCodeToURL(url); // Gets a URL from a likely embed code.
+    if (!url) return null;
+  }
+  let urlified;
+  try {
+    urlified = new URL(url);
+  } catch (e) { return null; }
+  if ((urlified.hostname !== 'google.com' && urlified.hostname !== 'www.google.com') || (urlified.pathname !== '/maps/embed' && urlified.pathname !== '/maps/embed/'))
+    return null;
+  return decodeURIComponent(urlified.searchParams.get('pb') || '') || null;
 }
 
 /**
@@ -57,7 +61,7 @@ export function campusMapURLToLocation(url) {
  *   For example, passing the location `'432490'` returns `'https://www.colorado.edu/map/?id=336#!m/432490'`.
  */
 export function campusMapLocationToURL(location) {
-	return 'https://www.colorado.edu/map/?id=336' + (location ? '#!m/' + location : '');
+  return 'https://www.colorado.edu/map/?id=336' + (location ? '#!m/' + location : '');
 }
 
 /**
@@ -66,5 +70,5 @@ export function campusMapLocationToURL(location) {
  *   The entire Google Maps URL when given just the location.
  */
  export function googleMapLocationToURL(location) {
-	return 'https://www.google.com/maps/embed?pb=' + encodeURIComponent(location);
+  return 'https://www.google.com/maps/embed?pb=' + encodeURIComponent(location);
 }
